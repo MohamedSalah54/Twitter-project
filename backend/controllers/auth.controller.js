359a95da9cccfd7769d197caa8a53d1826bd527a
MohamedSalah54 <mohamed.salah199988@gmail.com>
2024-09-27 18:03:29 +0300

    first commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+export const signup = async(req,res) =>{
+    res.json({
+        data:"you hit the signup endpoing"
+    });
+}
+
+
+export const signin = async(req,res) =>{
+    res.json({
+        data:"you hit the signin endpoing"
+    });
+}
+
+
+export const logout = async(req,res) =>{
+    res.json({
+        data:"you hit the logout endpoing"
+    });
+}
